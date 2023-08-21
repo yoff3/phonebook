e30471d850ded75bb9752e46795fd6a6a46ccb09
yoffei <tatazaha.iam@gmail.com>
2023-08-21 15:43:21 +0700

    added BackButton component
    added BackButton component
    installed react-icons

---
+++ phonebookrj/src/BackButton.jsx
@@ -0,0 +1,10 @@
+import {FaArrowLeft} from "react-icons/fa"
+
+export function BackButton(){
+    return (
+        <button className="btn btn-back">
+            <div className="icon-back"><FaArrowLeft/></div> Назад
+        </button>
+    )
+
+}
