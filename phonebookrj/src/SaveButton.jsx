1e4e07e90139e4b1180d381622d449435c0b208e
yoffei <tatazaha.iam@gmail.com>
2023-08-21 16:05:15 +0700

    Added SaveButton component
    added SaveButton component

---
+++ phonebookrj/src/SaveButton.jsx
@@ -0,0 +1,10 @@
+import {FaSave} from "react-icons/fa"
+
+export function SaveButton(){
+    return (
+        <button className="btn btn-save">
+            <div className="icon-save"><FaSave/></div> Сохранить
+        </button>
+    )
+
+}
